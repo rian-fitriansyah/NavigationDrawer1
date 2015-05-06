de65428ebf5d323b6e92396e328b2f392ea76872
nefrisuswita <nefrisuswita@gmail.com>
2015-05-06 11:32:30 +0700

    Update ListJadwalAdapter.java

--- app/src/main/java/com/example/android/navigationdrawerexample/Controller/ListJadwalAdapter.java
+++ app/src/main/java/com/example/android/navigationdrawerexample/Controller/ListJadwalAdapter.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.navigationdrawerexample.R;
@@ -16,16 +15,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by lenovo on 3/31/2015.
+ * Created by lenovo on 5/2/2015.
  */
-public class ListJadwalAdapter extends BaseAdapter {
-
+public class ListHadirAdapter extends BaseAdapter {
     private JSONArray dataArray;
     private Activity activity;
 
     private static LayoutInflater inflater = null;
 
-    public ListJadwalAdapter(JSONArray jsonArray, Activity a)
+    public ListHadirAdapter(JSONArray jsonArray, Activity a)
     {
         this.dataArray = jsonArray;
         this.activity = a;
@@ -51,63 +49,30 @@ public class ListJadwalAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
-        // set up convert view if it is null
         ListCell cell;
-        if (convertView == null)
-        {
-            convertView = inflater.inflate(R.layout.list_jadwal_cell, null);
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.list_hadir_cell, null);
             cell = new ListCell();
 
-            cell.Judul = (TextView) convertView.findViewById(R.id.judul);
-            cell.Asisten = (TextView) convertView.findViewById(R.id.asisten);;
-            cell.Tanggal = (TextView) convertView.findViewById(R.id.tanggal);
-            cell.Waktu = (TextView) convertView.findViewById(R.id.waktu);
-            cell.Ruangan = (TextView) convertView.findViewById(R.id.ruangan);
-
-            cell.mobile = (ImageView) convertView.findViewById(R.id.customer_mobile);
+            cell.Nama = (TextView) convertView.findViewById(R.id.username_mahasiswa);
 
             convertView.setTag(cell);
         }
-        else
-        {
+        else {
             cell = (ListCell) convertView.getTag();
         }
-
-        // change the data of cell
-
-        try
-        {
+        try {
             JSONObject jsonObject = this.dataArray.getJSONObject(position);
-            cell.Judul.setText(jsonObject.getString("Judul"));
-            cell.Asisten.setText(jsonObject.getString("Username"));
-            cell.Tanggal.setText(jsonObject.getString("Tanggal"));
-            cell.Waktu.setText(jsonObject.getString("W_Mulai") + " - " + jsonObject.getString("W_Akhir"));
-            cell.Ruangan.setText(jsonObject.getString("Ruangan"));
-
-            cell.mobile.setImageResource(R.drawable.avahome);
+            cell.Nama.setText(jsonObject.getString("Username"));
 
         }
-        catch (JSONException e)
-        {
+        catch (JSONException e) {
             e.printStackTrace();
         }
-
-
         return convertView;
     }
 
-
-
-    private  class  ListCell
-    {
-        private TextView Judul;
-        private TextView Asisten;
-        private TextView Tanggal;
-        private TextView Waktu;
-        private TextView Ruangan;
-
-        private ImageView mobile;
-
+    private  class  ListCell {
+        private TextView Nama;
     }
 }
