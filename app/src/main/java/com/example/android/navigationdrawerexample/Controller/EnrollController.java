77baaede39d97040845f4e48f0017108e65cb1be
nefrisuswita <nefrisuswita@gmail.com>
2015-05-06 10:48:13 +0700

    Update EnrollController.java

--- app/src/main/java/com/example/android/navigationdrawerexample/Controller/EnrollController.java
+++ app/src/main/java/com/example/android/navigationdrawerexample/Controller/EnrollController.java
@@ -1,6 +1,7 @@
 package com.example.android.navigationdrawerexample.Controller;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -13,8 +14,8 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
-import com.example.android.navigationdrawerexample.Model.Kelas;
-import com.example.android.navigationdrawerexample.R;
+import com.example.lenovo.pplnefri.Model.Kelas;
+import com.example.lenovo.pplnefri.R;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -26,6 +27,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,7 +42,6 @@ import java.util.List;
 public class EnrollController extends Activity{
     private LinearLayout linearMain;
     private String username;
-    private JSONArray jsonArray;
     private ArrayList<CheckBox> addKelas = new ArrayList<CheckBox>();
     private ArrayList<Kelas> pilihan = new ArrayList<Kelas>();
 
@@ -55,7 +57,7 @@ public class EnrollController extends Activity{
         linearMain = (LinearLayout) findViewById(R.id.container);
         Button enroll = (Button) findViewById(R.id.button);
 
-        new GetAllKelasTask().execute(linearMain);
+        new GetAllKelasTask(EnrollController.this).execute(linearMain);
 
         enroll.setOnClickListener(new View.OnClickListener() {
 
@@ -65,17 +67,15 @@ public class EnrollController extends Activity{
                 for (int i = 0; i < addKelas.size(); i++) {
                     if (addKelas.get(i).isChecked()) {
                         kelas += pilihan.get(addKelas.get(i).getId()).getId() + " ";
-                        Toast.makeText(getApplicationContext(), ""+ pilihan.get(addKelas.get(i).getId()).getId(), Toast.LENGTH_LONG).show();
+                        //Toast.makeText(getApplicationContext(), ""+ pilihan.get(addKelas.get(i).getId()).getId(), Toast.LENGTH_LONG).show();
                     }
                 }
                 if (!kelas.equals("")){
                     kelas = kelas.substring(0, kelas.length() - 1);
                     addEnroll(username, kelas);
-                    Toast.makeText(getApplicationContext(), "Masuk Enroll", Toast.LENGTH_LONG).show();
                 }
 
                 Intent showDetails = new Intent(getApplicationContext(), EnrollController.class);
-                //asumsi username gak null
                 showDetails.putExtra("Username", username);
                 startActivity(showDetails);
                 finish();
@@ -83,12 +83,42 @@ public class EnrollController extends Activity{
         });
     }
 
+    public List<String> getAllAsdosKelas(int id){
+        List<String> listDaftarAsdos = new ArrayList<String>();
+        String url = "http://ppl-a08.cs.ui.ac.id/enroll.php?fun=getAsdos&Id_Kelas=" + id;
+        JSONArray jsonArray = (new JSONParser()).getJSONArrayFromUrl(url);
+
+        if(jsonArray != null){
+            for (int i = 0; i < jsonArray.length(); i++) {
+                try {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    listDaftarAsdos.add(jsonObject.getString("Username"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }return listDaftarAsdos;
+    }
+
     private class GetAllKelasTask extends AsyncTask<LinearLayout,Long,LinearLayout>
     {
-        ArrayList a;
+        private ProgressDialog dialog;
+        private EnrollController activity;
+
+        public GetAllKelasTask(EnrollController activity) {
+            this.activity = activity;
+            dialog = new ProgressDialog(this.activity);
+        }
+
         @Override
         protected LinearLayout doInBackground(LinearLayout... params) {
             return params[0];
+        }
+
+        protected void onPreExecute() {
+            this.dialog.setMessage("Sedang mengambil data...");
+            this.dialog.show();
+            this.dialog.setCancelable(false);
         }
 
         @Override
@@ -106,11 +136,14 @@ public class EnrollController extends Activity{
                     CheckBox checkBox = new CheckBox(getApplicationContext());
                     checkBox.setId(i);
                     checkBox.setText(pilihan.get(i).getNama());
-                    checkBox.setTextColor(getResources().getColor(R.color.black));
+                    checkBox.setTextColor(getResources().getColor(R.color.dim_foreground_material_dark));
                     linearLayout3.addView(checkBox);
                     addKelas.add(checkBox);
                 } scrollView.addView(linearLayout3);
                 a.addView(scrollView);
+            }
+            if (dialog.isShowing()) {
+                dialog.dismiss();
             }
         }
     }
@@ -134,6 +167,34 @@ public class EnrollController extends Activity{
 
             String msg = "Berhasil";
             Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            Log.e("Client Protocol", "Log_Tag");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e("Log_Tag", "IOException");
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteEnroll(final String username, final String kelas) {
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+        nameValuePairs.add(new BasicNameValuePair("Kelas", kelas));
+        nameValuePairs.add(new BasicNameValuePair("Username", username));
+
+        InputStream is = null;
+
+        try {
+            HttpClient httpClient = new DefaultHttpClient();
+
+            HttpPost httpPost = new HttpPost("http://ppl-a08.cs.ui.ac.id/deleteEnroll.php");
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+
+            is = entity.getContent();
+
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (ClientProtocolException e) {
